0ada73034cbe75c5f5728c2ec62b1ebcc31a97d2
madketsy <90768530+madketsy@users.noreply.github.com>
2025-12-10 00:11:24 +0300

    Update queries.sql
    Первый отчет о десятке лучших продавцов. Таблица состоит из трех колонок - данных о продавце, суммарной выручке с проданных товаров и количестве проведенных сделок, и отсортирована по убыванию выручки

--- queries.sql
+++ queries.sql
@@ -1,3 +1,15 @@
 select 
    Count(c.customer_id) as customers_count
+
 from customers c 
+
+SELECT
+    e.first_name || ' ' || e.last_name AS seller,
+    COUNT(*) AS operations,
+    SUM(s.quantity * p.price) AS income
+FROM sales s
+JOIN employees e ON s.sales_person_id = e.employee_id
+JOIN products p ON s.product_id = p.product_id
+GROUP BY e.employee_id
+ORDER BY income DESC
+LIMIT 10;
